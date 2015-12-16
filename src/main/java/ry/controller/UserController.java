1cac30833c416528e941ce2431c61c1b224b34c1
wry0127 <wry0127@outlook.com>
2015-12-16 23:32:18 +0800

    20151216

---
+++ src/main/java/ry/controller/UserController.java
@@ -0,0 +1,26 @@
+package ry.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ry.model.User;
+import ry.service.UserService;
+
+@Controller
+@RequestMapping("/userController")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/showUser")
+	public String showUser(String id,HttpServletRequest request) {
+		User u = userService.findUserById(id);
+		request.setAttribute("user", u);
+		return "showUser";
+	}
+}
